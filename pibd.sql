0c69a1487214d3f82c3c618fbde82da02c01e13e
Cletus2000 <64313378+Cletus2000@users.noreply.github.com>
2023-11-30 23:32:03 +0100

    mas fotos

--- pibd.sql
+++ pibd.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 30-11-2023 a las 20:44:31
+-- Tiempo de generación: 30-11-2023 a las 23:31:37
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -94,7 +94,13 @@ INSERT INTO `fotos` (`idFoto`, `titulo`, `descripcion`, `fecha`, `pais`, `album`
 (11, 'Carl-woman', 'Carl elástico', '2023-11-11', 4, 1, 'carl-woman.webp', 'carl-elastigirl', '2023-11-30 18:42:45'),
 (12, 'Bob esponja emo', 'Pobresito moviesponja, ta triste', '2023-11-05', 5, 2, 'emo-spongebob.gif', 'bob esponja emo', '2023-11-30 18:42:45'),
 (13, 'Chinochin bebelin', 'Chinochin bebelin de parte de julito', '2023-08-17', 2, 2, 'chino.png', 'chino.png', '2023-11-30 19:26:03'),
-(14, 'Steve Harvey', 'Señor patata Steve Harvey', '2024-01-18', 4, 2, 'don-papa.jpg', 'don-papa.jpg', '2023-11-30 19:27:59');
+(14, 'Steve Harvey', 'Señor patata Steve Harvey', '2024-01-18', 4, 2, 'don-papa.jpg', 'don-papa.jpg', '2023-11-30 19:27:59'),
+(15, 'Don pollo', 'Quien quiela peldel su tiempo que lo pielda, yo me via come ete pollo, en 3 minuto', '2023-11-19', 4, 1, 'donpollo.jpg', 'donpollo.jpg', '2023-11-30 22:31:08'),
+(16, 'Pablo Motos', 'Que entre la china', '1945-11-20', 3, 2, 'pablomotos.jpg', 'pablomotos.jpg', '2023-11-30 22:31:08'),
+(17, 'Pou', 'Un gnomo estaba andando encima do asfalto', '2023-11-29', 7, 2, 'pou.png', 'pou.png', '2023-11-30 22:31:08'),
+(18, 'Torrente no hay mas que uno', 'Mucho moro', '2013-08-19', 3, 1, 'santiago-segura.jpg', 'santiago-segura.jpg', '2023-11-30 22:31:08'),
+(19, 'Shrek - Minion', 'Tulepera con la papaya', '2023-11-26', 10, 2, 'shrek-minion.png', 'shrek-minion.png', '2023-11-30 22:31:08'),
+(20, '._.', 'º-º', '2023-06-23', 1, 1, 'unknown.png', 'unknown.png', '2023-11-30 22:31:08');
 
 -- --------------------------------------------------------
 
@@ -240,7 +246,7 @@ ALTER TABLE `estilos`
 -- AUTO_INCREMENT de la tabla `fotos`
 --
 ALTER TABLE `fotos`
-  MODIFY `idFoto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `idFoto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT de la tabla `paises`
